52b4278a55bd0446c122569be8e6d5b7bb6bbb65
Tanmoy Parvez <tanmoyparvez23@gmail.com>
2025-08-10 19:27:39 +0600

    add: openai api key

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import openai
+from dotenv import load_dotenv
+import os
+load_dotenv()  # Reads .env file and loads variables into environment
 
-openai.api_key=""
+
+openai.api_key=os.getenv("OPENAI_API_KEY")
 
 def chat_with_gpt(prompt):
     res = openai.ChatCompletion.create()
-        model="gpt-5"
+        model="gpt-5",
         messages=[{
             "role":"user", "content":prompt
         }]
     return res.choices[0].message.content.strip()
+
